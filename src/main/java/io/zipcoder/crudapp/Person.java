d6d3a96607de51a5adbc8d42c1cd4f4608e8e757
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-06-15 15:38:16 -0400

    personclass

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,54 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.criteria.CriteriaBuilder;
+
+/**
+ * Created by aurorabanuelos on 6/15/17.
+ */
+@Entity
+public class Person {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private int age;
+
+    public Person(Integer id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
