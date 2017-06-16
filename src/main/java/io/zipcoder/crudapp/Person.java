9233cf4883efc92a40a8f479a7004b556279ea8c
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-06-16 11:55:08 -0400

    udpate

--- src/main/java/io/zipcoder/crudapp/Person.java
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -20,7 +20,6 @@ public class Person {
     private String name;
     private int age;
 
-    @Autowired
     public Person(String name, int age){
         this.name = name;
         this.age = age;
--- src/main/java/io/zipcoder/crudapp/RequestController.java
+++ src/main/java/io/zipcoder/crudapp/RequestController.java
@@ -2,10 +2,7 @@ package io.zipcoder.crudapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.xml.ws.Response;
 
 /**
  * Created by aurorabanuelos on 6/15/17.
